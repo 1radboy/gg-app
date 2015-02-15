fef43685b90339061eb6052aa2700865b52f0dde
Stephen Clark <stephen@californiaclarks.com>
2015-02-15 08:10:03 -0800

    Adding missing Recipe files.

---
+++ GroceryGuru/src/com/californiaclarks/groceryguru/Recipe.java
@@ -0,0 +1,48 @@
+package com.californiaclarks.groceryguru;
+
+import java.util.Locale;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class Recipe extends Fragment {
+
+	TextView tvName, tvContent;
+	String name;
+	String content;
+
+	// called when items are updates
+	public void setRecipe(JSONObject json) {
+		try {
+			name = json.getJSONObject("recipe").getString("name");
+			content = json.getJSONObject("recipe").getString("content");
+			tvName.setText(name.toUpperCase(Locale.US));
+			tvContent.setText(content);
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		super.onCreateView(inflater, container, savedInstanceState);
+
+		View vFrag = inflater.inflate(R.layout.recipe, container, false);
+		tvName = (TextView) vFrag.findViewById(R.id.tvName);
+		tvContent = (TextView) vFrag.findViewById(R.id.tvContent);
+		((GroceryGuru) getActivity()).refreshRecipe();
+		tvName.setText(name.toUpperCase(Locale.US));
+		tvContent.setText(content);
+
+		return vFrag;
+	}
+
+}
